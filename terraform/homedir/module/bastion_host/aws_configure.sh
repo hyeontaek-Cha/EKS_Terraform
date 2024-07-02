93742c9d738e0a09cdd63862a1c6bd6f3eb15f63
hyeontaek-Cha <84235819+hyeontaek-Cha@users.noreply.github.com>
2024-07-03 00:19:23 +0900

    update

--- terraform/homedir/module/bastion_host/aws_configure.sh
+++ terraform/homedir/module/bastion_host/aws_configure.sh
@@ -2,9 +2,9 @@
 
 echo aws configure `whoami` : 
 
-aws configure set aws_access_key_id YOUR_ACCESS_KEY_ID
-aws configure set aws_secret_access_key YOUR_SECRET_ACCESS_KEY
-aws configure set default.region MY_REGION
+aws configure set aws_access_key_id <YOUR_ACCESS_KEY_ID>
+aws configure set aws_secret_access_key <YOUR_SECRET_ACCESS_KEY>
+aws configure set default.region <MY_REGION>
 aws configure set output json
 
 cat $HOME/.aws/credentials

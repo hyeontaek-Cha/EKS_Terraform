9566bb014b16ff88fa6570558cc0851ea1c3b392
hyeontaek-Cha <84235819+hyeontaek-Cha@users.noreply.github.com>
2024-07-05 17:38:34 +0900

    update

--- terraform/module/vpc/main.tf
+++ terraform/module/vpc/main.tf
@@ -80,7 +80,7 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_nat_gateway" "nat_gw" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnets[0].id
+  subnet_id     = aws_subnet.public_subnets[1].id
 
   tags = {
     Name = "cht-eks-nat-gateway"
